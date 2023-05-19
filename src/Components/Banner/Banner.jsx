f007a33f7f308f90cb01850e7676f8ddb06a76cc
BF Ibrahim <mdibrahim36194@gmail.com>
2023-05-19 14:55:25 +0600

    Gallery added

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Banner.css'
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
@@ -9,7 +10,7 @@ const Banner = () => {
                     <h1 className='text-5xl mb-3 font-bold'>Welcome To BF Toy <span className='text-red-600'>Cars</span></h1>
                     <p>"Rev up the excitement with BF Toy Cars! Explore our collection of high-quality toy cars for all ages. Unleash your inner racer and fuel your imagination today!"</p>
 
-                    <button className=' mt-4 mb-4 btn bg-red-600 border-none'>View Toys</button>
+                    <Link to='/alltoys'><button className=' mt-4 mb-4 btn bg-red-600 border-none'>View All Toys</button></Link>
                 </div>
 
                 <div className='banner-Image hidden lg:block'>
---
+++ src/Components/Gallery/Gallery.jsx
@@ -0,0 +1,135 @@
+import React from 'react';
+
+const Gallery = () => {
+    return (
+        <div>
+            <div className='text-center'>
+                <h1 className='text-3xl text-red-600 text-center mt-20 font-bold'>Toys Gallery</h1>
+                <p className='mb-10'>Just See our Best Collection from here. This Is Our Toys Gallery</p>
+            </div>
+            <div className='md:flex md:justify-evenly'>
+                <div className="card md:w-96 bg-orange-50 shadow-xl p-4 mb-6">
+                    <figure><img src="https://m.media-amazon.com/images/I/41-FTW+LZvL._AC_.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Sports Car</h2>
+                        <p className="font-bold text-green-500">Price: $110</p>
+                        <div className='flex'>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+                        </div>
+                        <div className="card-actions justify-end">
+                            <a target='_blank' href="https://www.amazon.ca/Aosom-Compatible-Battery-Powered-Electric-Parental/dp/B09CDHWBLS">                            <button className="btn  bg-red-600 border-none">Buy Now</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="card md:w-96 bg-orange-50 shadow-xl p-4  mb-6">
+                    <figure><img src="https://m.media-amazon.com/images/I/61U214eflkL._AC_UF894,1000_QL80_.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Laxurious Sports Car</h2>
+                        <p className="font-bold text-green-500">Price: $99</p>
+                        <div className='flex'>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+
+                            <span><img className='w-5 me-2' src="https://i.postimg.cc/sxLznRnx/download-removebg-preview.png" alt="" /></span>
+                        </div>
+                        <div className="card-actions justify-end">
+                            <a target='_blank' href="https://www.amazon.com/Licensed-Lamborghini-Electric-Suspension-Bluetooth/dp/B09W1WBJ6R?th=1"><button className="btn bg-red-600 border-none">Buy Now</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="card md:w-96 bg-orange-50 shadow-xl p-4  mb-6">
+                    <figure><img src="https://m.media-amazon.com/images/I/81Y4hHPYzyL._AC_UF894,1000_QL80_.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Daron UPS truck</h2>
+                        <p className="font-bold text-green-500">Price: $87</p>
+                        <div className='flex'>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+
+                            <span><img className='w-5 me-2' src="https://i.postimg.cc/sxLznRnx/download-removebg-preview.png" alt="" /></span>
+
+                            <span><img className='w-5 me-2' src="https://i.postimg.cc/J0HT3wSR/yellow-star-icon-10-removebg-preview.png" alt="" /></span>
+
+                        </div>
+                        <div className="card-actions justify-end">
+                            <a target='_blank' href="https://www.amazon.com/Daron-UPS-Pullback-Package-Truck/dp/B008VD9BQM"><button className='btn bg-red-600 border-none'>Buy Now</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='md:flex md:justify-evenly'>
+                <div className="card md:w-96 bg-orange-50 shadow-xl p-4 mb-6">
+                    <figure><img src="https://m.media-amazon.com/images/I/71yyoiSDS-S._AC_UF894,1000_QL80_.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Lamborgini Toy</h2>
+                        <p className="font-bold text-green-500">Price: $120</p>
+                        <div className='flex'>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+                        </div>
+                        <div className="card-actions justify-end">
+                            <a target="_blank" href="https://www.amazon.com/Metal-Model-Light-Sound-Green/dp/B09289WQDH?th=1">   <button className="btn  bg-red-600 border-none">Buy Now</button></a>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="card md:w-96 bg-orange-50 shadow-xl p-4  mb-6">
+                    <figure><img src="https://m.media-amazon.com/images/I/71YB465WIqL._AC_SL1500_.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Laxurious Car</h2>
+                        <p className="font-bold text-green-500">Price: $100</p>
+                        <div className='flex'>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+
+                            <span><img className='w-5 me-2' src="https://i.postimg.cc/sxLznRnx/download-removebg-preview.png" alt="" /></span>
+                        </div>
+                        <div className="card-actions justify-end">
+                            <a target='_blank' href="https://www.amazon.ca/BDTCTK-Lexus-Off-Road-Luxury-Model/dp/B08YY4R942"><button className="btn  bg-red-600 border-none">Buy Now</button></a>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="card md:w-96 bg-orange-50 shadow-xl p-4  mb-6">
+                    <figure><img src="https://rukminim1.flixcart.com/image/416/416/vehicle-pull-along/g/w/y/electric-police-car-toy-for-kids-with-flashing-lights-and-sirens-original-imaerf8aq54tg2gz.jpeg?q=70" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Police Car</h2>
+                        <p className="font-bold text-green-500">Price: $80</p>
+                        <div className='flex'>
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+
+                            <span><img className='w-5 me-2' src="https://img.uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-icon.png" alt="" /></span>
+
+                            <span><img className='w-5 me-2' src="https://i.postimg.cc/sxLznRnx/download-removebg-preview.png" alt="" /></span>
+
+                            <span><img className='w-5 me-2' src="https://i.postimg.cc/J0HT3wSR/yellow-star-icon-10-removebg-preview.png" alt="" /></span>
+
+                        </div>
+                        <div className="card-actions justify-end">
+                            <a target="_blank" href="https://www.flipkart.com/memtes-electric-police-car-toy-kids-flashing-lights-sirens-sounds-bump-go-action-2-front-doors-open/p/itmer36jptbzsydb">                            <button className="btn bg-red-600 border-none">Buy Now</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Gallery;
