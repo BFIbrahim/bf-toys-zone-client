a40f47f98dba795ab182debec3d5350922f7ec89
BF Ibrahim <mdibrahim36194@gmail.com>
2023-05-19 09:23:49 +0600

    Banner added

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div className='bg-orange-100'>
+            <div className='md:flex w-5/6 m-auto justify-center header-wrap'>
+                <div className='align-middle'>
+                    <h1 className='text-5xl mb-3 font-bold'>Welcome To BF Toy <span className='text-red-600'>Cars</span></h1>
+                    <p>"Rev up the excitement with BF Toy Cars! Explore our collection of high-quality toy cars for all ages. Unleash your inner racer and fuel your imagination today!"</p>
+
+                    <button className=' mt-4 mb-4 btn bg-red-600 border-none'>View Toys</button>
+                </div>
+
+                <div className='banner-Image hidden lg:block'>
+                    <img src="https://i.postimg.cc/3NLjp9Zj/Header-Car-PNG.png" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -4,25 +4,26 @@ import './Navigation.css'
 
 const Navigation = () => {
     return (
-        <div className="navbar flex justify-between align-middle bg-orange-100">
-            <div>
-                <h1 className='text-3xl font-bold'>
-                    <span className='text-blue-500'>BF</span>
-                    <span className='text-black ml-2'>Toy</span>
-                    <span className='text-red-600 ml-2'>Cars</span>
-                </h1>
-            </div>
+        <div className='bg-orange-100 '>
+            <div className="navbar flex justify-between align-middle md:px-10">
+                <div>
+                    <h1 className='md:text-3xl font-bold flex'>
+                        <img className='w-10' src="https://i.postimg.cc/3JYs9CGQ/Logo.png" alt="" />
+                        <span className='text-red-600 ml-2'>Cars</span>
+                    </h1>
+                </div>
 
-            <div className=' flex gap-4 font-bold nav-content text-gray-600'>
-                <Link to="/">All Toys</Link>
-                <Link to='/mytoys'>My Toys</Link>
-                <Link to="/blog">Blog</Link>
-            </div>
+                <div className=' flex gap-4 font-bold nav-content text-gray-600'>
+                    <Link to="/">All Toys</Link>
+                    <Link to='/mytoys'>My Toys</Link>
+                    <Link to="/blog">Blog</Link>
+                </div>
 
-            <div>
-                <img className='w-10 h-10 ml-4' src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" />
-            </div>
+                <div>
+                    <img className='w-10 h-10 ml-4' src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" />
+                </div>
 
+            </div>
         </div>
     );
 };
