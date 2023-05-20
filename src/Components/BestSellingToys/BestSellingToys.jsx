2d5d747399746771db4e9a8609bdb3f86306e8e4
BF Ibrahim <mdibrahim36194@gmail.com>
2023-05-20 07:49:27 +0600

    best selling section added

--- src/Components/BestSellingToys/BestSellingToys.jsx
+++ src/Components/BestSellingToys/BestSellingToys.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const BestSellingToys = () => {
     return (
@@ -7,19 +8,21 @@ const BestSellingToys = () => {
             <p>People like This Kind of toys below. You can choose from here</p>
 
             <div className='md:flex justify-evenly mt-10'>
-                <div className="card card-compact w-60 shadow-xl">
+                <div className="card card-compact md:w-60 shadow-xl mb-3">
                     <figure><img src="https://m.media-amazon.com/images/I/61VGdLGf2iL.jpg" alt="Shoes" /></figure>
                 </div>
-                <div className="card card-compact md:w-60 shadow-xl ">
+                <div className="card card-compact md:w-60 shadow-xl mb-3 ">
                     <figure><img src="https://i5.walmartimages.com/asr/0a0417d3-e35e-45fa-b026-a2e5bf76d23c.3ba15f20d1a90112cca9b5f8334231ae.jpeg?odnHeight=450&odnWidth=450&odnBg=ffffff" alt="Shoes" /></figure>
                 </div>
-                <div className="card card-compact w-60 shadow-xl ">
+                <div className="card card-compact md:w-60 shadow-xl  mb-3">
                     <figure><img src="https://ae01.alicdn.com/kf/HTB1eCx1VhjaK1RjSZFAq6zdLFXar/Baby-Car-Two-seat-Children-s-Electric-Unisex-Four-wheeled-Car-Off-road-Remote-Control-Toy.jpg_640x640.jpg" alt="Shoes" /></figure>
                 </div>
-                <div className="card card-compact w-60 shadow-xl ">
+                <div className="card card-compact md:w-60 shadow-xl  mb-3">
                     <figure><img src="https://m.media-amazon.com/images/I/71jrSZJjiHL.jpg" alt="Shoes" /></figure>
                 </div>
             </div>
+            
+            <Link to='/alltoys'><button className=' mt-5 mb-42 btn bg-red-600 border-none'>View All Toys</button></Link>
         </div>
     );
 };
